acf56b2fbeaa99ddbb3dc367d58f468f0bccf30c
Bn Split <bnsplit00@gmail.com>
2024-07-07 19:24:45 +0000

    first commit

--- launch.sh
+++ launch.sh
@@ -1,1 +1,2 @@
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+#!/usr/bin/bash
+sh -c "$(wget https://raw.githubusercontent.com/BnSplits/anime_fetcher/main/source/anime_fetcher.sh -O -)"
