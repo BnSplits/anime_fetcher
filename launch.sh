d1b417704ccd2c4c465a35675e3ca955171eb252
Bn Split <159204088+BnSplits@users.noreply.github.com>
2024-09-15 00:45:27 +0000

    Delete launch.sh

--- launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-# Installation de dépendances npm
-echo "Installation de dépendances npm..."
-npm list puppeteer inquirer nanospinner gradient-string >/dev/null 2>&1 || npm install puppeteer@latest @inquirer/prompts@latest nanospinner@latest gradient-string@latest >/dev/null 2>&1
-
-# Exécution du script
-node ./src/script.js
-clear
